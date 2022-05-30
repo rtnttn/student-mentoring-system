1322ca24d29953b80fc1bdad89e701d2bf5020bf
rtnttn <31089484+rtnttn@users.noreply.github.com>
2022-05-30 13:38:12 +1000

    manage subjects dash button

--- client/src/components/subjects/Subjects.js
+++ client/src/components/subjects/Subjects.js
@@ -22,8 +22,9 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash, FaArrowLeft } from 'react-icons/fa';
 import classnames from 'classnames';
+import { Link } from 'react-router-dom';
 import { getSubjects, addSubject, deleteSubject } from '../../actions/subjectActions';
 
 const Subjects = ({ subjects, getSubjects, addSubject, deleteSubject, loading }) => {
@@ -70,6 +71,12 @@ const Subjects = ({ subjects, getSubjects, addSubject, deleteSubject, loading })
       <div className="container ps-5 pe-5 pt-3">
         <div className="ps-5 pe-5">
           <h4 className="text-center">Add a Subject</h4>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <h4 className="text-primary">
+              <FaArrowLeft className="m-1" />
+              Back
+            </h4>
+          </Link>
           <form onSubmit={(e) => onSubmit(e)}>
             <div className="row g-2 mb-1">
               <div className="col-sm-2">
